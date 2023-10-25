7fbf2f7a6bfcb695de6f8a510a11ce8505d085be
Archie Martin <archie.cgm@gmail.com>
2023-10-25 09:36:55 +0100

    styling and HeroImage prep

--- App.tsx
+++ App.tsx
@@ -1,29 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native';
 import formatNumberWithCommas from './utils/formatNumberWithCommas';
+import HeroImage from './components/HeroImage';
 
 // replicating value received from backend, assuming an integer value
+const FOOTPRINT = 7565
 const END_OF_YEAR_TARGET_VALUE = 5855
 const LEFT_TO_CUT_VALUE = 409
 const TWENTY_THIRTY_TARGET_VALUE = 2500
 
 export default function App() {
-  const endOfYearValue = formatNumberWithCommas(END_OF_YEAR_TARGET_VALUE)
+  const endOfYearTargetValue = formatNumberWithCommas(END_OF_YEAR_TARGET_VALUE)
   const leftToCutValue = formatNumberWithCommas(LEFT_TO_CUT_VALUE)
-  const twentyThirtyValue = formatNumberWithCommas(TWENTY_THIRTY_TARGET_VALUE)
+  const twentyThirtyTargetValue = formatNumberWithCommas(TWENTY_THIRTY_TARGET_VALUE)
+
   return (
     <View style={styles.container}>
+      <HeroImage />
       <View style={styles.estimationContainer}>
         <View style={{width: 100}}>
           <Text style={[styles.pink, styles.text]}>End of year target</Text>
-          <Text style={[styles.purple, styles.text]}>{endOfYearValue}<Text style={{}}> kg</Text></Text>
+          <Text 
+            style={[{fontSize: 25}, styles.purple, styles.text]}
+          >
+            {endOfYearTargetValue}
+            <Text style={{fontWeight: 'bold', fontSize: 12}}
+            > kg
+            </Text>
+          </Text>
+          <Text style={{fontSize: 8, color: '#5e2b68', ...styles.text}}>
+            of carbon emissions
+          </Text>
         </View>
         <View style={{width: 100}}>
-          <Text style={[styles.pink, styles.text]}>Left to cut this year</Text>
-          <Text style={[{fontSize: 25}, styles.purple, styles.text]}>{leftToCutValue}<Text style={{fontWeight: 'bold'}}> kg</Text></Text>
+          <Text 
+            style={[styles.pink, styles.text]}
+          >
+            Left to cut this year
+          </Text>
+          <Text style={[{fontSize: 25, color: '#5e2b68'}, styles.text]}>
+            {leftToCutValue}
+            <Text style={{fontWeight: 'bold', fontSize: 12}}
+            > kg
+            </Text>
+          </Text>
+          <Text style={{fontSize: 8, color: '#5e2b68', ...styles.text}}>
+            of carbon emissions
+          </Text>
         </View>
       </View>
       <View style={[styles.twentyThirtyTarget]}>
-        <Text style={styles.twentyThirtyTargetInner}>Your 2030 target is {twentyThirtyValue} kg</Text>
+        <Text style={styles.twentyThirtyTargetInner}>Your 2030 target is {twentyThirtyTargetValue} kg</Text>
       </View>
     </View>
   );
@@ -35,22 +61,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 25
   },
   estimationContainer: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    gap: 10
+    gap: 10,
   },
   text: {
     textAlign: 'center',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   twentyThirtyTarget: {
-    padding: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     backgroundColor: '#ebd7ee',
     borderRadius: 100,
-    fontWeight: 700,
+    fontWeight: 'bold',
   },
   twentyThirtyTargetInner: {
     color: '#5e2b68',
