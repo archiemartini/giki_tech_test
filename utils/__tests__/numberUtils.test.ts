b40affe8b833dad039f64b31693bdf3b99f02cb2
Archie Martin <archie.cgm@gmail.com>
2023-10-26 16:14:59 +0100

    number utils test

---
+++ utils/__tests__/numberUtils.test.ts
@@ -0,0 +1,27 @@
+import { formatNumberWithCommas } from '../numberUtils';
+
+describe('formatNumberWithCommas', () => {
+  it('should format a number with commas', () => {
+    const number = 123456789;
+    const formattedNumber = formatNumberWithCommas(number);
+    expect(formattedNumber).toBe('123,456,789');
+  });
+
+  it('should return the same number if it is less than 1000', () => {
+    const number = 999;
+    const formattedNumber = formatNumberWithCommas(number);
+    expect(formattedNumber).toBe('999');
+  });
+
+  it('should handle negative numbers', () => {
+    const number = -123456789;
+    const formattedNumber = formatNumberWithCommas(number);
+    expect(formattedNumber).toBe('-123,456,789');
+  });
+
+  it('should handle decimal numbers', () => {
+    const number = 1234.5678;
+    const formattedNumber = formatNumberWithCommas(number);
+    expect(formattedNumber).toBe('1,234.5678');
+  });
+});
---
+++ utils/numberUtils.ts
@@ -0,0 +1,4 @@
+export function formatNumberWithCommas(number: number): string {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
