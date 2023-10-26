5eb869f2a2b48c0c3532bc45c074f87b8e38f736
Archie Martin <archie.cgm@gmail.com>
2023-10-26 16:39:10 +0100

    extracted HomeScreen.tsx from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { formatNumberWithCommas } from "./utils/numberUtils";
-import Hero from "./components/Hero";
-import data from "./data.json";
-import EstimationInfo from "./components/EstimationInfo";
-import Target2030 from "./components/Target2030";
 import { useFonts, Poppins_700Bold } from "@expo-google-fonts/poppins";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
-  const footprintValue = formatNumberWithCommas(data.footprint);
-  const endOfYearTargetValue = formatNumberWithCommas(data.end_of_year_target);
-  const leftToCutValue = formatNumberWithCommas(data.left_to_cut);
-  const twentyThirtyTargetValue = formatNumberWithCommas(
-    data.twenty_thirty_target,
-  );
-
   let [fontsLoaded] = useFonts({
     Poppins_700Bold,
   });
@@ -28,14 +17,7 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Hero footprintValue={footprintValue} />
-      <EstimationInfo
-        endOfYearTarget={endOfYearTargetValue}
-        leftToCut={leftToCutValue}
-      />
-      <Target2030 twentyThirtyTarget={twentyThirtyTargetValue} />
-    </View>
+    <HomeScreen />
   );
 }
 
@@ -45,6 +27,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    gap: 25,
   },
 });
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { formatNumberWithCommas } from "../utils/numberUtils";
+import Hero from "../components/Hero";
+import data from "../data.json";
+import EstimationInfo from "../components/EstimationInfo";
+import Target2030 from "../components/Target2030";
+
+export default function HomeScreen() {
+  const footprintValue = formatNumberWithCommas(data.footprint);
+  const endOfYearTargetValue = formatNumberWithCommas(data.end_of_year_target);
+  const leftToCutValue = formatNumberWithCommas(data.left_to_cut);
+  const twentyThirtyTargetValue = formatNumberWithCommas(
+    data.twenty_thirty_target,
+  );
+
+  return (
+    <View style={styles.container}>
+      <Hero footprintValue={footprintValue} />
+      <EstimationInfo
+        endOfYearTarget={endOfYearTargetValue}
+        leftToCut={leftToCutValue}
+      />
+      <Target2030 twentyThirtyTarget={twentyThirtyTargetValue} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 25,
+  },
+});
+
